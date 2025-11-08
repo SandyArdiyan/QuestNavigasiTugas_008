9505309ff86d71303afd052ffcfb29d23e50f48b
SandyArdiyan <sanardiyan881@gmail.com>
2025-11-08 23:04:58 +0700

    perbaiki cara Anda memanggil konstruktor di PesertaViewModel.kt

--- app/src/main/java/com/example/pertemuan6tugas/models/PesertaViewModel.kt
+++ app/src/main/java/com/example/pertemuan6tugas/models/PesertaViewModel.kt
@@ -1,27 +1,42 @@
-package com.example.pertemuan6tugas.viewmodel
+package com.example.pertemuan6tugas.models
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
-import com.example.pertemuan6tugas.model.Peserta
+import com.example.pertemuan6tugas.model.Peserta // Import data class Peserta
 
 class PesertaViewModel : ViewModel() {
 
-    // Daftar ini akan otomatis memperbarui UI saat ada perubahan
+    // Daftar ini akan otomatis memperbarui UI
     val pesertaList = mutableStateListOf<Peserta>()
 
     init {
-        // Langsung isi dengan data contoh saat ViewModel pertama kali dibuat
+        // Data contoh awal
         pesertaList.addAll(
             listOf(
-                Peserta("Asroni Sukirman", "Laki-Laki", "Cerai", "Bantul"),
-                Peserta("Aprilia Kurnianti", "Perempuan", "Lajang", "Bantul"),
-                Peserta("Haris Setyawan", "Laki-Laki", "Kawin", "Jogja")
+                Peserta(
+                    nama = "Harun Sukirman",
+                    gender = "Laki-laki",
+                    status = "Ceraf",
+                    alamat = "Bantul"
+                ),
+                Peserta(
+                    nama = "Aprilla Nurdianti",
+                    gender = "Perempuan",
+                    status = "Lajang",
+                    alamat = "Bantul"
+                ),
+                Peserta(
+                    nama = "Haris Setyawan",
+                    gender = "Laki-laki",
+                    status = "Kawin",
+                    alamat = "Jogja"
+                )
             )
         )
     }
 
-    fun tambahPeserta(pesertaBaru: Peserta) {
-        pesertaList.add(pesertaBaru)
+    // Fungsi untuk menambah data dari formulir
+    fun tambahPeserta(peserta: Peserta) {
+        pesertaList.add(peserta)
     }
 }
-
