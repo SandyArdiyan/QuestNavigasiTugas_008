5c8ed78d8821e3d016cdc758b072b7f80544dba3
SandyArdiyan <sanardiyan881@gmail.com>
2025-11-08 20:56:07 +0700

    Menambhkan Navigasi Compose di build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
