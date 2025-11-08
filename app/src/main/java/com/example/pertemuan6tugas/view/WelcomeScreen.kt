2b75489b99f6d1fbe4942c9076f957b6be59712d
SandyArdiyan <sanardiyan881@gmail.com>
2025-11-08 20:57:33 +0700

    Menambhkan Tampilan WelcomeScreen.kt

--- app/src/main/java/com/example/pertemuan6tugas/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/pertemuan6tugas/view/WelcomeScreen.kt
@@ -1,2 +1,37 @@
 package com.example.pertemuan6tugas.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WelcomeScreen(
+    onNavigateToList: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Selamat Datang",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "CARD-LST Mobile App 2025")
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(onClick = onNavigateToList) {
+            Text("Lihat Daftar Peserta")
+        }
+    }
+}
